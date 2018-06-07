483f6b9f491536111a2058d2c6bfe40115de3543
jngalletto <jngalletto@gmail.com>
2018-06-06 22:34:06 -0300

    WIP home screen

---
+++ SwiftBaseProject/Entities/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by Joaquin Galletto on 6/6/18.
+//  Copyright © 2018 Joaquin Galletto. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    
+    let id: String
+    let title: String
+    let overview: String
+    let voteAverage: Double
+    let popularity: Double
+    let posterPath: String
+    let adult: Bool
+    let releaseDate: String
+    let runtime: String
+}
+
--- SwiftBaseProject/SupportFiles/Constants.swift
+++ SwiftBaseProject/SupportFiles/Constants.swift
@@ -9,9 +9,10 @@
 import Foundation
 
 struct Constants {
+    static let api_key = "0ce27b34f3d404a535fff6ebf9460694"
   // Sample env management
   #if STAGING
-    static let apiBaseUrl = URL(string: "https://google.com")!
+    static let apiBaseUrl = URL(string: "https://api.themoviedb.org/3")!
   #else
     static let apiBaseUrl = URL(string: "point_to_prod_here")!
   #endif
